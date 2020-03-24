118de313ed2808f0ea903b478506881217529599
whddus9509 <whddus9509@naver.com>
2020-03-25 00:52:52 +0900

    update

--- board/src/main/java/board/configuration/DatabaseConfiguration.java
+++ board/src/main/java/board/configuration/DatabaseConfiguration.java
@@ -40,11 +40,18 @@ public class DatabaseConfiguration {
 		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
 		sqlSessionFactoryBean.setDataSource(dataSource);
 		sqlSessionFactoryBean.setMapperLocations(applicationContext.getResources("classpath:/mapper/**/sql-*.xml"));
+		sqlSessionFactoryBean.setConfiguration(mybatisConfig());
 		return sqlSessionFactoryBean.getObject();
 	}
 	
 	@Bean
 	public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
 		return new SqlSessionTemplate(sqlSessionFactory);
+	}
+	
+	@Bean
+	@ConfigurationProperties(prefix="mybatis.configuration")
+	public org.apache.ibatis.session.Configuration mybatisConfig(){
+		return new org.apache.ibatis.session.Configuration();
 	}
 }
---
+++ board/src/main/java/board/dto/BoardDto.java
@@ -0,0 +1,19 @@
+package board.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
+public class BoardDto {
+	private int boardIdx;
+	private String title;
+	private String contents;
+	private int hitCnt;
+	private String creatorId;
+	private LocalDateTime createdDatetime;
+	private String updaterId;
+	private LocalDateTime updatedDatetime;
+	
+
+}
