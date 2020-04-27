3e1100875f5b464a2a26e708c2ceacc07ceda98b
whddus9509 <whddus9509@naver.com>
2020-04-27 18:22:05 +0900

    log

--- board/src/main/java/board/controller/BoardController.java
+++ board/src/main/java/board/controller/BoardController.java
@@ -2,6 +2,8 @@ package board.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,12 +15,15 @@ import board.service.BoardService;
 
 @Controller
 public class BoardController {
+	private Logger log = LoggerFactory.getLogger(this.getClass());
+	
 	@Autowired
 	private BoardService boardService;
 	
 	@RequestMapping("/board/openBoardList.do")
 	public ModelAndView openBoardList() throws Exception{
 		ModelAndView mv= new ModelAndView("/board/boardList");
+		log.debug("openBoardList");
 		List<BoardDto> list =boardService.selectBoardList();
 		mv.addObject("list",list);
 		return mv;
