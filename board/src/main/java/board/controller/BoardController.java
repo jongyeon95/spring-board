a3e36416367cba47e2a75256f21144b1b26523f1
whddus9509 <whddus9509@naver.com>
2020-05-30 23:20:46 +0900

    Create logger

--- board/src/main/java/board/controller/BoardController.java
+++ board/src/main/java/board/controller/BoardController.java
@@ -2,6 +2,8 @@ package board.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,12 +15,14 @@ import board.service.BoardService;
 
 @Controller
 public class BoardController {
+	private Logger log=LoggerFactory.getLogger(this.getClass());
 	
 	@Autowired
 	private BoardService boardService;
 	
 	@RequestMapping("/board/openBoardList.do")
 	public ModelAndView openBoardList() throws Exception{
+		log.debug("openBoardList");
 		ModelAndView mv=new ModelAndView("/board/boardList");
 		
 		List<BoardDto> list= boardService.selectBoardList();
