1489b85dae69c2874627f5a56f693cacb73f7937
whddus9509 <whddus9509@naver.com>
2020-05-31 16:08:41 +0900

    create aop

---
+++ board/src/main/java/board/aop/LoggerAspect.java
@@ -0,0 +1,36 @@
+package board.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LoggerAspect {
+	private Logger log=LoggerFactory.getLogger(this.getClass());
+	
+	@Around("execution(* board..controller.*Controller.*(..)) or"
+			+ "execution(* board..service.*Impl.*(..)) or"
+			+ "execution(* board..dao.*Mapper.*(..)")
+	public Object logPrint(ProceedingJoinPoint joinPoint) throws Throwable{
+		String type ="";
+		String name=joinPoint.getSignature().getDeclaringTypeName();
+		if(name.indexOf("Controller") > -1) {
+			type="Controller \t: ";
+		}
+		else if(name.indexOf("Service")> -1) {
+			type="Service \t: ";
+			
+		}
+		else if(name.indexOf("Mapper") > -1) {
+			type="Mapper \t\t:";
+		}
+		log.debug(type+name+"."+joinPoint.getSignature().getName()+"()");
+		return joinPoint.proceed();
+	}
+	
+
+}
