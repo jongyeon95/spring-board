245ce4cf1a263fc1d92002b1e992019e98a22ad0
whddus9509 <whddus9509@naver.com>
2020-06-08 21:43:04 +0900

    create filter

--- board/src/main/java/board/configuration/WebMvcConfiguration.java
+++ board/src/main/java/board/configuration/WebMvcConfiguration.java
@@ -1,6 +1,14 @@
 package board.configuration;
 
+import java.nio.charset.Charset;
+
+import javax.servlet.Filter;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,6 +20,21 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(new LoggerInterceptor());
+	}
+	
+	@Bean
+	public Filter characterEncodingFilter() {
+		CharacterEncodingFilter characterEncodingFilter= new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		
+		return characterEncodingFilter;
+		
+	}
+	
+	@Bean
+	public HttpMessageConverter<String> responseBodyConverter(){
+		return new StringHttpMessageConverter(Charset.forName("UTF-8"));
 	}
 	
 	
