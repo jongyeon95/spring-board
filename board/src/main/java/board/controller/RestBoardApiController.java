6e4fc8b7e0aa817593c23ce1c9bccb88771cbe30
whddus9509 <whddus9509@naver.com>
2020-06-21 13:10:26 +0900

    swagger

--- board/src/main/java/board/controller/RestBoardApiController.java
+++ board/src/main/java/board/controller/RestBoardApiController.java
@@ -11,34 +11,41 @@ import org.springframework.web.bind.annotation.RestController;
 
 import board.dto.BoardDto;
 import board.service.BoardService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
+@Api(description="게시판 REST API")
 @RestController
 public class RestBoardApiController {
 	
 	@Autowired
 	private BoardService boardservice;
 	
+	@ApiOperation(value ="게시글 목록 조회")
 	@RequestMapping(value="/api/board", method=RequestMethod.GET)
 	public List<BoardDto> openBoardList() throws Exception{
 		return boardservice.selectBoardList();
 	}
 	
+	@ApiOperation(value ="게시글 작성")
 	@RequestMapping(value="/api/board/write", method=RequestMethod.POST)
 	public void insertBoard(@RequestBody BoardDto board) throws Exception{
 		boardservice.insertBoard(board, null);
 	}
 	
+	@ApiOperation(value ="게시글 상세 내용 조회")
 	@RequestMapping(value="/api/board/{boardIdx}", method=RequestMethod.GET)
 	public BoardDto openBoardDetail(@PathVariable("boardIdx") int boardIdx) throws Exception{
 		return boardservice.selectBoardDetail(boardIdx);
 	}
 	
+	@ApiOperation(value ="게시글 상세 내용 수정")
 	@RequestMapping(value="/api/board/{boardIdx}", method=RequestMethod.PUT)
 	public String updateBoard(@RequestBody BoardDto board) throws Exception{
 		boardservice.updateBoard(board);
 		return "redirect:/board";
 	}
-	
+	@ApiOperation(value ="게시글 삭제")
 	@RequestMapping(value="/api/board/{boardIdx}", method=RequestMethod.DELETE)
 	public String deleteBoard(@PathVariable("boardIdx") int boardIdx) throws Exception{
 		boardservice.deleteBoard(boardIdx);
--- board/src/main/java/board/dto/BoardDto.java
+++ board/src/main/java/board/dto/BoardDto.java
@@ -3,17 +3,38 @@ package board.dto;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+
+@ApiModel(value="BoardDto : 게시글 내용" , description="게시글 내용")
 @Data
 public class BoardDto {
+	@ApiModelProperty(value="게시글 번호")
 	private int boardIdx;
+	
+	@ApiModelProperty(value="게시글 제목")
 	private String title;
+	
+	@ApiModelProperty(value="게시글 내용")
 	private String contents;
+	
+	@ApiModelProperty(value="조회수")
 	private int hitCnt;
+	
+	@ApiModelProperty(value="작성자 아이디")
 	private String creatorId;
+	
+	@ApiModelProperty(value="작성 시간")
 	private LocalDateTime createdDatetime;
+	
+	@ApiModelProperty(value="수정자 아이디")
 	private String updaterId;
+	
+	@ApiModelProperty(value="수정 시간")
 	private LocalDateTime updatedDatetime;
+	
+	@ApiModelProperty(value="첨부파일 목록")
 	private List<BoardFileDto> fileList;
 
 }
